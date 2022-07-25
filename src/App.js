cd9b194e1ff8ab610e32a9073713769e2ef42e71
Zachary Truong <zacharytruong@gmail.com>
2022-07-24 21:44:07 -0500

    project with ssr

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { StrictMode, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+      </ThemeContext.Provider>
+    </StrictMode>
+  );
+};
+
+export default App;
---
+++ src/ClientApp.js
@@ -0,0 +1,10 @@
+import { hydrate } from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+hydrate(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
